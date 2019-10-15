56c7cad83e0a76223595b5571e072f821f54fec4
matx17 <matx17@student.bth.se>
2019-10-15 17:15:33 +0200

    final changes

---
+++ cilent.py
@@ -0,0 +1,29 @@
+import socket,select,sys
+from sys import argv
+s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+script,host,port,nickname = argv
+list = int(port)
+Host=""
+port=list
+s.connect(("", port))
+print('connect to server')
+NICK = nickname
+s.send(NICK)
+while 1:
+    Socket_list=[sys.stdin,s]
+    read_list,write_list,error_list=select.select(Socket_list,[],[])
+    for k in read_list:
+        if k ==s:
+            message =s.recv(1024)
+            print(message)
+            if not message :
+                print("disconnect")
+                exit()
+        else:
+            message = sys.stdin.readline()
+            s.send(message.encode('utf-8'))
+            sys.stdout.write("me:")
+            sys.stdout.write(message)
+            sys.stdout.flush()
+
+s.close()
---
+++ server.py
@@ -0,0 +1,63 @@
+import socket
+from threading import Thread
+from sys import argv
+s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+script,host,port=argv
+list = int(port)
+Host=""
+port=list
+clients={}
+cli_socket=[]
+s.bind(("",port))
+
+def broadcast(message, cli_conn):
+    for k in cli_socket:
+         if k != s and k != cli_conn :
+            k.send(message)
+
+def acpt_clients():
+    while 1:
+        conn,addr=s.accept()
+        print("connected with", addr)
+        conn.send("HELLO 2 \n".encode('utf-8'))
+        cli_socket.append(conn)
+        Thread(target=handler, args=(conn,addr)).start()
+
+
+
+def  handler(conn, addr):
+    data = conn.recv(1024)
+    name = data.strip("NICK ")
+    if len(name)<=10:
+       conn.send("Ok \n")
+    else:
+       conn.send("ERROR - Length of the nickname is greater than 10 \n")
+    message = "%s connect to chat \n"%name
+    broadcast(message,conn)
+    clients[conn] =data
+
+
+    while 1:
+        message= conn.recv(1024)
+        msg = message.strip("MSG ")
+        if msg == 'quits':
+            conn.close()
+            del clients[conn]
+            mail="%s left from chat \n"% name
+            broadcast(mail,conn)
+            break
+
+        else :
+            if len(message) >200:
+                conn.send("ERROR \n")
+            else:
+                mail_data =name + ":" + msg      
+            broadcast(mail_data,conn)
+while True:
+      s.listen(100)
+      print("connection is waiting")
+      iThread = Thread(target=acpt_clients)
+      iThread.daemon = True
+      iThread.start()
+      iThread.join()
+s.close()
