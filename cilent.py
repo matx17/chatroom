712feb1a4c234d364d41f3a31e6ccb27666ac8cb
matx17 <matx17@student.bth.se>
2019-10-23 16:26:16 +0200

    input from command line as requested

--- cilent.py
+++ cilent.py
@@ -1,14 +1,15 @@
-import socket,select,sys
-from sys import argv
+import socket,select,sys,re
 s=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-script,host,port,nickname = argv
-list = int(port)
-Host=""
-port=list
-s.connect(("", port))
+if (len(sys.argv)!=3):
+   print("How to use in terminal-> python chatclient.py serveripadd:port nick")
+args = str(sys.argv[1]).split(':')
+host = str(args[0])
+port = int(args[1])
+nick = str(sys.argv[2])
+s.connect((host, port))
 print('connect to server')
-NICK = nickname
-s.send(NICK)
+
+s.send(('NICK '+nick).encode('utf-8'))
 while 1:
     Socket_list=[sys.stdin,s]
     read_list,write_list,error_list=select.select(Socket_list,[],[])
