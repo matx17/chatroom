64c6c2f4c1dc63b0882c58b8a4e28dd1bcabf169
matx17 <matx17@student.bth.se>
2019-10-23 19:45:21 +0200

    change in broadcast

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-import socket,re
+import socket,re,sys
 from threading import Thread
 s=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 if (len(sys.argv)!=2):
@@ -8,6 +8,7 @@ host = str(args[0])
 port = int(args[1])
 clients_dict={}
 cli_socket=[]
+cli_socket_temp=[]
 cli_socker_perm=[]
 s.bind((host,port))
 
@@ -30,51 +31,56 @@ def acpt_clients():
         print("connected with", addr)
         conn.send("HELLO 1 \n".encode('utf-8'))
         cli_socket.append(conn)
+        cli_socket_temp.append(conn)
         Thread(target=handler, args=(conn,addr)).start()
 
 
 
-def  handler(conn, addr):
-     try:
-        nick = conn.recv(1024).decode('utf-8')
-        if nick:
-           find = re.search(r'NICK\s(\S*)',nick)
-           name = str(find.group(1))
-           if len(name)>12:
-              conn.send("Error-> your nickname shouls be less than 12 characters")
-           elif re.search(r'#',name) or re.search(r'\$',name) or re.search(r'!',name) or re.search(r'@',name) or re.search(r'\*',name):
-              conn.send("Error-> Don't use special characters i your nickname")
-           elif find:
-              conn.send("%s connect to chat \n"%name)
-              cli_socket_perm.append(conn)
-              clients_dict[conn]=name
-           else: 
-              conn.send("Error-> Actual command protocal is NICK <nick>")
-        else:
+def handler(conn, addr):
+    while True:
+     if conn in cli_socket_temp:
+       try:
+          nick = conn.recv(1024).decode('utf-8')
+          if nick:
+             find = re.search(r'NICK\s(\S*)',nick)
+             name = str(find.group(1))
+             if len(name)>12:
+                conn.send("Error-> your nickname shouls be less than 12 characters")
+             elif re.search(r'#',name) or re.search(r'\$',name) or re.search(r'!',name) or re.search(r'@',name) or re.search(r'\*',name):
+                conn.send("Error-> Don't use special characters i your nickname")
+             elif find:
+                conn.send("%s connect to chat \n"%name)
+                cli_socket_perm.append(conn)
+                clients_dict[conn]=name
+                cli_socket_temp.remove(conn)
+             else: 
+                conn.send("Error-> Actual command protocal is NICK <nick>")
+          else:
+                conn.close()
+                cli_socket.remove(conn) 
+                cli_socket_temp.remove(conn)
+       except:
+          continue
+     elif conn in cli_socket_perm:
+
+       try:	
+          message= conn.recv(1024).decode('utf-8')
+          if message:
+              find = re.search(r'MSG\s',message)
+              if len(message)>250:
+                  conn.send("Error-> message should not exceed 250 char")
+              elif find:
+                  msg = 'MSG '+str(clients_dict[conn])+':'+message[4:]
+                  broadcast(msg,conn)
+              else:
+                  conn.send("Error Actual command is MSG <msg>")
+          else: 
               conn.close()
               cli_socket.remove(conn)
-     except:
-         continue
-
-  while 1:
-     try:	
-        message= conn.recv(1024).decode('utf-8')
-        if message:
-            find = re.search(r'MSG\s',message)
-            if len(message)>250:
-                conn.send("Error-> message should not exceed 250 char")
-            elif find:
-                msg = 'MSG '+str(clients_dict[conn])+':'message[4:]
-                broadcast(msg,conn)
-            else:
-                conn.send("Error Actual command is MSG <msg>")
-        else: 
-            conn.close()
-            cli_socket.remove(conn)
-            cli_socket_perm.remove(conn)
-            del clients_dict[conn]
-     except:
-        continue
+              cli_socket_perm.remove(conn)
+              del clients_dict[conn]
+       except:
+          continue
 while True:
       s.listen(100)
       print("connection is waiting")
