bd1c33e513543af72906bff4d6c1f0ac287feb7e
matx17 <matx17@student.bth.se>
2019-10-27 18:55:50 +0100

    change at strip function

--- server.py
+++ server.py
@@ -43,7 +43,7 @@ def acpt_clients():
           	   nick = conn.recv(1024).decode('utf-8')
           	   if nick:
                       find = re.match('^NICK\s(\s*)',nick)
-                      name = nick.strip("NICK ")
+                      name = nick.lstrip("NICK")
                       if len(name)>12:
                 	 conn.send("Error-> your nickname shouls be less than 12 characters")
                       elif((re.search('\#',name) != None) or (re.search('\$',name) != None) or (re.search('\!',name) != None)  or (re.search('\@',name) != None) or (re.search('\*',name) != None) or (re.search('\^',name) != None) or (re.search('\%',name) != None) or (re.search('\&',name) != None)):
@@ -53,10 +53,7 @@ def acpt_clients():
                          cli_socket_perm.append(conn)
                          clients_dict[conn]=name
                          cli_socket_temp.remove(conn)
-            	      
-                     
-             	      
-                      else: 
+            	      else: 
                          conn.send("Error-> malformed command,Actual command is NICK <nick>")
                    else:
                       conn.close()
